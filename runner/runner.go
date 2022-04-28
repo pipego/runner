537e69c74662b1cd4eff77e9331ae6076678569a
Jia <angersax@sina.com>
2022-04-28 20:07:04 +0800

    feat(server): add support for dag

--- runner/runner.go
+++ runner/runner.go
@@ -1,7 +1,14 @@
+// Package runner implements a directed acyclic graph task runner with deterministic teardown.
+// it is similar to package errgroup, in that it runs multiple tasks in parallel and returns
+// the first error it encounters. Users define a Runner as a set vertices (functions) and edges
+// between them. During Run, the directed acyclec graph will be validated and each vertex
+// will run in parallel as soon as it's dependencies have been resolved. The Runner will only
+// return after all running goroutines have stopped.
 package runner
 
 import (
 	"context"
+	"errors"
 
 	"github.com/pipego/runner/config"
 )
@@ -14,9 +21,22 @@ type Config struct {
 	Config config.Config
 }
 
+// Runner collects functions and arranges them as vertices and edges of a directed acyclic graph.
+// Upon validation of the graph, functions are run in parallel topological order. The zero value
+// is useful.
 type runner struct {
-	cfg *Config
+	cfg   *Config
+	fn    map[string]func() error
+	graph map[string][]string
 }
+
+type result struct {
+	err  error
+	name string
+}
+
+var errMissingVertex = errors.New("missing vertex")
+var errCycleDetected = errors.New("dependency cycle detected")
 
 func New(_ context.Context, cfg *Config) Runner {
 	return &runner{
@@ -28,6 +48,134 @@ func DefaultConfig() *Config {
 	return &Config{}
 }
 
+// Run will validate that all edges in the graph point to existing vertices, and that there are
+// no dependency cycles. After validation, each vertex will be run, deterministically, in parallel
+// topological order. If any vertex returns an error, no more vertices will be scheduled and
+// Run will exit and return that error once all in-flight functions finish execution.
 func (r *runner) Run() error {
-	return nil
+	// sanity check
+	if len(r.fn) == 0 {
+		return nil
+	}
+	// count how many deps each vertex has
+	deps := make(map[string]int)
+	for vertex, edges := range r.graph {
+		// every vertex along every edge must have an associated fn
+		if _, ok := r.fn[vertex]; !ok {
+			return errMissingVertex
+		}
+		for _, vertex := range edges {
+			if _, ok := r.fn[vertex]; !ok {
+				return errMissingVertex
+			}
+			deps[vertex]++
+		}
+	}
+
+	if r.detectCycles() {
+		return errCycleDetected
+	}
+
+	running := 0
+	resc := make(chan result, len(r.fn))
+	var err error
+
+	// start any vertex that has no deps
+	for name := range r.fn {
+		if deps[name] == 0 {
+			running++
+			r.start(name, r.fn[name], resc)
+		}
+	}
+
+	// wait for all running work to complete
+	for running > 0 {
+		res := <-resc
+		running--
+
+		// capture the first error
+		if res.err != nil && err == nil {
+			err = res.err
+		}
+
+		// don't enqueue any more work on if there's been an error
+		if err != nil {
+			continue
+		}
+
+		// start any vertex whose deps are fully resolved
+		for _, vertex := range r.graph[res.name] {
+			deps[vertex]--
+			if deps[vertex] == 0 {
+				running++
+				r.start(vertex, r.fn[vertex], resc)
+			}
+		}
+	}
+	return err
+}
+
+// AddVertex adds a function as a vertex in the graph. Only functions which have been added in this
+// way will be executed during Run.
+func (r *runner) AddVertex(name string, fn func() error) {
+	if r.fn == nil {
+		r.fn = make(map[string]func() error)
+	}
+
+	r.fn[name] = fn
+}
+
+// AddEdge establishes a dependency between two vertices in the graph. Both from and to must exist
+// in the graph, or Run will err. The vertex at from will execute before the vertex at to.
+func (r *runner) AddEdge(from, to string) {
+	if r.graph == nil {
+		r.graph = make(map[string][]string)
+	}
+
+	r.graph[from] = append(r.graph[from], to)
+}
+
+func (r *runner) detectCycles() bool {
+	visited := make(map[string]bool)
+	recStack := make(map[string]bool)
+
+	for vertex := range r.graph {
+		if !visited[vertex] {
+			if r.detectCyclesHelper(vertex, visited, recStack) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (r *runner) detectCyclesHelper(vertex string, visited, recStack map[string]bool) bool {
+	visited[vertex] = true
+	recStack[vertex] = true
+
+	for _, v := range r.graph[vertex] {
+		// only check cycles on a vertex one time
+		if !visited[v] {
+			if r.detectCyclesHelper(v, visited, recStack) {
+				return true
+			}
+			// if we've visited this vertex in this recursion stack, then we have a cycle
+		} else if recStack[v] {
+			return true
+		}
+	}
+
+	recStack[vertex] = false
+
+	return false
+}
+
+func (r *runner) start(name string, fn func() error, resc chan<- result) {
+	go func() {
+		resc <- result{
+			name: name,
+			err:  fn(),
+		}
+	}()
 }
--- runner/runner_test.go
+++ runner/runner_test.go
@@ -1,9 +1,228 @@
 package runner
 
 import (
+	"errors"
 	"testing"
+	"time"
 )
 
-func TestRunner(t *testing.T) {
-	// TODO
+func TestZero(t *testing.T) {
+	var r runner
+
+	res := make(chan error)
+	go func() { res <- r.Run() }()
+
+	select {
+	case err := <-res:
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestOne(t *testing.T) {
+	var r runner
+
+	err := errors.New("error")
+	r.AddVertex("one", func() error { return err })
+
+	res := make(chan error)
+	go func() { res <- r.Run() }()
+
+	select {
+	case err := <-res:
+		if want, have := err, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestManyNoDeps(t *testing.T) {
+	var r runner
+
+	err := errors.New("error")
+	r.AddVertex("one", func() error { return err })
+	r.AddVertex("two", func() error { return nil })
+	r.AddVertex("three", func() error { return nil })
+	r.AddVertex("fout", func() error { return nil })
+
+	res := make(chan error)
+	go func() { res <- r.Run() }()
+
+	select {
+	case err := <-res:
+		if want, have := err, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestManyWithCycle(t *testing.T) {
+	var r runner
+
+	r.AddVertex("one", func() error { return nil })
+	r.AddVertex("two", func() error { return nil })
+	r.AddVertex("three", func() error { return nil })
+	r.AddVertex("four", func() error { return nil })
+
+	r.AddEdge("one", "two")
+	r.AddEdge("two", "three")
+	r.AddEdge("three", "four")
+	r.AddEdge("three", "one")
+
+	res := make(chan error)
+	go func() { res <- r.Run() }()
+
+	select {
+	case err := <-res:
+		if want, have := errCycleDetected, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestInvalidToVertex(t *testing.T) {
+	var r runner
+
+	r.AddVertex("one", func() error { return nil })
+	r.AddVertex("two", func() error { return nil })
+	r.AddVertex("three", func() error { return nil })
+	r.AddVertex("four", func() error { return nil })
+
+	r.AddEdge("one", "two")
+	r.AddEdge("two", "three")
+	r.AddEdge("three", "four")
+	r.AddEdge("three", "definitely-not-a-valid-vertex")
+
+	res := make(chan error)
+	go func() { res <- r.Run() }()
+
+	select {
+	case err := <-res:
+		if want, have := errMissingVertex, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestInvalidFromVertex(t *testing.T) {
+	var r runner
+
+	r.AddVertex("one", func() error { return nil })
+	r.AddVertex("two", func() error { return nil })
+	r.AddVertex("three", func() error { return nil })
+	r.AddVertex("four", func() error { return nil })
+
+	r.AddEdge("one", "two")
+	r.AddEdge("two", "three")
+	r.AddEdge("three", "four")
+	r.AddEdge("definitely-not-a-valid-vertex", "three")
+
+	res := make(chan error)
+	go func() { res <- r.Run() }()
+
+	select {
+	case err := <-res:
+		if want, have := errMissingVertex, err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+}
+
+func TestManyWithDepsSuccess(t *testing.T) {
+	var r runner
+
+	res := make(chan string, 7)
+	r.AddVertex("one", func() error {
+		res <- "one"
+		return nil
+	})
+	r.AddVertex("two", func() error {
+		res <- "two"
+		return nil
+	})
+	r.AddVertex("three", func() error {
+		res <- "three"
+		return nil
+	})
+	r.AddVertex("four", func() error {
+		res <- "four"
+		return nil
+	})
+	r.AddVertex("five", func() error {
+		res <- "five"
+		return nil
+	})
+	r.AddVertex("six", func() error {
+		res <- "six"
+		return nil
+	})
+	r.AddVertex("seven", func() error {
+		res <- "seven"
+		return nil
+	})
+
+	r.AddEdge("one", "two")
+	r.AddEdge("one", "three")
+	r.AddEdge("two", "four")
+	r.AddEdge("two", "seven")
+	r.AddEdge("five", "six")
+
+	err := make(chan error)
+	go func() { err <- r.Run() }()
+
+	select {
+	case err := <-err:
+		if want, have := error(nil), err; want != have {
+			t.Errorf("want %v, have %v", want, have)
+		}
+	case <-time.After(100 * time.Millisecond):
+		t.Error("timeout")
+	}
+
+	results := make([]string, 7)
+	timeout := time.After(100 * time.Millisecond)
+
+	for i := range results {
+		select {
+		case results[i] = <-res:
+		case <-timeout:
+			t.Error("timeout")
+		}
+	}
+
+	checkOrder("one", "two", results, t)
+	checkOrder("one", "three", results, t)
+	checkOrder("two", "four", results, t)
+	checkOrder("two", "seven", results, t)
+	checkOrder("five", "six", results, t)
+}
+
+func checkOrder(from, to string, results []string, t *testing.T) {
+	var fromIndex, toIndex int
+
+	for i := range results {
+		if results[i] == from {
+			fromIndex = i
+		}
+		if results[i] == to {
+			toIndex = i
+		}
+	}
+
+	if fromIndex > toIndex {
+		t.Errorf("from vertex: %s came after to vertex: %s", from, to)
+	}
 }
