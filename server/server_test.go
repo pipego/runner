c4813576f4e2e1c6026e305a60963b6fd26c660e
Jia <angersax@sina.com>
2022-08-08 08:58:25 +0800

    eat(server): add test cases for running script file

--- server/server_test.go
+++ server/server_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"compress/gzip"
 	"context"
 	"os"
 	"testing"
@@ -8,6 +10,25 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
+
+func initZip(data []byte) ([]byte, error) {
+	var b bytes.Buffer
+
+	gz := gzip.NewWriter(&b)
+	defer func(gz *gzip.Writer) {
+		_ = gz.Close()
+	}(gz)
+
+	if _, err := gz.Write(data); err != nil {
+		return nil, err
+	}
+
+	if err := gz.Flush(); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
 
 func TestInitRunner(t *testing.T) {
 	s := server{
@@ -18,7 +39,7 @@ func TestInitRunner(t *testing.T) {
 	assert.Equal(t, nil, err)
 }
 
-func TestLoadFile(t *testing.T) {
+func TestLoadUnzipped(t *testing.T) {
 	s := server{
 		cfg: DefaultConfig(),
 	}
@@ -40,4 +61,36 @@ func TestLoadFile(t *testing.T) {
 	buf = []byte("echo \"Hello World!\"")
 	_, err = s.loadFile(ctx, buf, false)
 	assert.NotEqual(t, nil, err)
+}
+
+func TestLoadZipped(t *testing.T) {
+	s := server{
+		cfg: DefaultConfig(),
+	}
+
+	ctx := context.Background()
+
+	err := s.initFile(ctx)
+	assert.Equal(t, nil, err)
+
+	buf := []byte("#!/bin/bash\necho \"Hello World!\"")
+	buf, err = initZip(buf)
+	assert.Equal(t, nil, err)
+
+	name, err := s.loadFile(ctx, buf, true)
+	assert.Equal(t, nil, err)
+
+	if _, err = os.Stat(name); errors.Is(err, os.ErrNotExist) {
+		assert.Error(t, err)
+	}
+	_ = s.cfg.File.Remove(ctx, name)
+
+	buf = []byte("echo \"Hello World!\"")
+	buf, err = initZip(buf)
+	assert.Equal(t, nil, err)
+
+	name, err = s.loadFile(ctx, buf, true)
+	assert.NotEqual(t, nil, err)
+
+	_ = s.cfg.File.Remove(ctx, name)
 }
