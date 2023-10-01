5ba22e4beec0e1ac83e99b025f05741a26e607cf
Jia Jia <angersax@sina.com>
2023-10-01 21:48:11 +0800

    feat(glance): add implementation for glance on dir, file and sys

--- glance/glance.go
+++ glance/glance.go
@@ -2,7 +2,19 @@ package glance
 
 import (
 	"context"
+	"fmt"
+	"math"
+	"net"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 
 	"github.com/pipego/runner/config"
 )
@@ -21,13 +33,34 @@ const (
 type Glance interface {
 	Init(context.Context) error
 	Deinit(context.Context) error
-	Dir(context.Context) error
-	File(context.Context) error
-	Sys(context.Context) error
+	Dir(context.Context, string) (int, []Entries, error)
+	File(context.Context, string, int) (bool, string, error)
+	Sys(context.Context) (Resource, Resource, Stats, Stats, Stats, string, string, error)
 }
 
 type Config struct {
 	Config config.Config
+}
+
+type Entries struct {
+	Group string `json:"group"`
+	Mode  string `json:"mode"`
+	Name  string `json:"name"`
+	Owner string `json:"owner"`
+	Size  int    `json:"size"`
+	Time  string `json:"time"`
+	Type  string `json:"type"`
+}
+
+type Resource struct {
+	MilliCPU int64 `json:"milliCPU"`
+	Memory   int64 `json:"memory"`
+	Storage  int64 `json:"storage"`
+}
+
+type Stats struct {
+	Total string `json:"total"`
+	Used  string `json:"used"`
 }
 
 type glance struct {
@@ -54,17 +87,120 @@ func (g *glance) Deinit(_ context.Context) error {
 	return nil
 }
 
-func (g *glance) Dir(ctx context.Context) error {
+func (g *glance) Dir(_ context.Context, path string) (total int, entries []Entries, err error) {
 	// TODO: FIXME
-	return nil
+	return 0, nil, nil
 }
 
-func (g *glance) File(ctx context.Context) error {
+func (g *glance) File(_ context.Context, path string, maxSize int) (readable bool, content string, err error) {
 	// TODO: FIXME
-	return nil
+	return false, "", nil
 }
 
-func (g *glance) Sys(ctx context.Context) error {
+// nolint: gocritic
+func (g *glance) Sys(_ context.Context) (allocatable, requested Resource, _cpu, _memory, _storage Stats, _host, _os string, err error) {
 	// TODO: FIXME
-	return nil
+	return allocatable, requested, _cpu, _memory, _storage, "", "", nil
+}
+
+func (g *glance) _host() string {
+	conn, _ := net.Dial("udp", "8.8.8.8:8")
+	defer func(conn net.Conn) {
+		_ = conn.Close()
+	}(conn)
+
+	buf := conn.LocalAddr().(*net.UDPAddr)
+
+	return strings.Split(buf.String(), ":")[0]
+}
+
+func (g *glance) milliCPU() (alloc, request int64) {
+	c, err := cpu.Counts(true)
+	if err != nil {
+		return -1, -1
+	}
+
+	if c*Milli > math.MaxInt64 {
+		return -1, -1
+	}
+
+	// FIXME: Got error on MacOS 10.13.6
+	p, err := cpu.Percent(Duration, false)
+	if err != nil {
+		return -1, -1
+	}
+
+	used := float64(c) * p[0] * 0.01
+	if used > math.MaxInt64 {
+		return -1, -1
+	}
+
+	return int64(c * Milli), int64(used * Milli)
+}
+
+func (g *glance) memory() (alloc, request int64) {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		return -1, -1
+	}
+
+	if v.Total > math.MaxInt64 || v.Used > math.MaxInt64 {
+		return -1, -1
+	}
+
+	return int64(v.Total), int64(v.Used)
+}
+
+func (g *glance) storage() (alloc, request int64) {
+	helper := func(path string) bool {
+		found := false
+		p, _ := disk.Partitions(false)
+		for _, item := range p {
+			if strings.HasPrefix(item.Device, Dev) && item.Mountpoint == path {
+				found = true
+				break
+			}
+		}
+		return found
+	}
+
+	r, err := disk.Usage(Root)
+	if err != nil {
+		return -1, -1
+	}
+
+	total := r.Total
+	used := r.Used
+
+	if helper(Home) {
+		h, err := disk.Usage(Home)
+		if err != nil {
+			return -1, -1
+		}
+		total = h.Total
+		used = h.Used
+	}
+
+	if total > math.MaxInt64 || used > math.MaxInt64 {
+		return -1, -1
+	}
+
+	return int64(total), int64(used)
+}
+
+func (g *glance) stats(alloc, req Resource) (_cpu, memory, storage Stats, _host, _os string) {
+	_cpu.Total = strconv.FormatInt(alloc.MilliCPU/Milli, Base) + " CPU"
+	_cpu.Used = strconv.FormatInt(req.MilliCPU*100/alloc.MilliCPU, Base) + "%"
+
+	info, _ := host.Info()
+	caser := cases.Title(language.BrazilianPortuguese)
+	_os = fmt.Sprintf("%s %s", caser.String(info.Platform), info.PlatformVersion)
+
+	memory.Total = strconv.FormatInt(alloc.Memory>>Bitwise, Base) + " GB"
+	memory.Used = strconv.FormatInt(req.Memory>>Bitwise, Base) + " GB"
+
+	storage.Total = strconv.FormatInt(alloc.Storage>>Bitwise, Base) + " GB"
+	storage.Used = strconv.FormatInt(req.Storage>>Bitwise, Base) + " GB"
+
+	return _cpu, memory, storage, _host, _os
 }
--- glance/glance_test.go
+++ glance/glance_test.go
@@ -7,13 +7,75 @@ import (
 )
 
 func TestDir(t *testing.T) {
+	// TODO: FIXME
 	assert.Equal(t, nil, nil)
 }
 
 func TestFile(t *testing.T) {
+	// TODO: FIXME
 	assert.Equal(t, nil, nil)
 }
 
 func TestSys(t *testing.T) {
+	// TODO: FIXME
 	assert.Equal(t, nil, nil)
+}
+
+func TestHost(t *testing.T) {
+	g := glance{
+		cfg: DefaultConfig(),
+	}
+
+	h := g._host()
+	assert.NotEqual(t, "", h)
+}
+
+func TestMilliCPU(t *testing.T) {
+	g := glance{
+		cfg: DefaultConfig(),
+	}
+
+	alloc, request := g.milliCPU()
+	assert.NotEqual(t, -1, alloc)
+	assert.NotEqual(t, -1, request)
+}
+
+func TestMemory(t *testing.T) {
+	g := glance{
+		cfg: DefaultConfig(),
+	}
+
+	alloc, request := g.memory()
+	assert.NotEqual(t, -1, alloc)
+	assert.NotEqual(t, -1, request)
+}
+
+func TestStorage(t *testing.T) {
+	g := glance{
+		cfg: DefaultConfig(),
+	}
+
+	alloc, request := g.storage()
+	assert.NotEqual(t, -1, alloc)
+	assert.NotEqual(t, -1, request)
+}
+
+func TestStats(t *testing.T) {
+	g := glance{
+		cfg: DefaultConfig(),
+	}
+
+	alloc := Resource{}
+	request := Resource{}
+
+	alloc.MilliCPU, request.MilliCPU = g.milliCPU()
+	alloc.Memory, request.Memory = g.memory()
+	alloc.Storage, request.Storage = g.storage()
+
+	_cpu, _memory, _storage, _host, _os := g.stats(alloc, request)
+	assert.NotEqual(t, nil, _cpu)
+	assert.NotEqual(t, nil, _memory)
+	assert.NotEqual(t, nil, _storage)
+	assert.NotEqual(t, "", _host)
+	assert.NotEqual(t, "", _os)
 }
