492a7908c7ec662de07b523be3d49a2fdc6286d8
Jia Jia <angersax@sina.com>
2023-10-05 10:01:48 +0800

    fix(glance): fix build error on darwin & linux

--- glance/stat_darwin.go
+++ glance/stat_darwin.go
@@ -5,13 +5,14 @@ package glance
 import (
 	"os"
 	"strconv"
+	"syscall"
 )
 
 func (g *glance) statId(info os.FileInfo) (uid, gid string) {
 	statt, _ := info.Sys().(*syscall.Stat_t)
 
-	uid := strconv.FormatUint(uint64(statt.Uid), Base)
-	gid := strconv.FormatUint(uint64(statt.Gid), Base)
+	uid = strconv.FormatUint(uint64(statt.Uid), Base)
+	gid = strconv.FormatUint(uint64(statt.Gid), Base)
 
 	return uid, gid
 }
