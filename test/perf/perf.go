6365e30b6712ea330c130e0d838f6704974c14ea
Jia Jia <angersax@sina.com>
2023-12-05 10:08:40 +0800

    feat(test): add files for perf testing

---
+++ test/perf/perf.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"sync"
+)
+
+const (
+	LEN = 5000
+	SEP = '\n'
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var lineChan = make(chan string, LEN)
+
+	cmd := exec.Command("python3", "perf.py")
+	cmd.Stderr = cmd.Stdout
+
+	pipe, _ := cmd.StdoutPipe()
+
+	reader := bufio.NewReader(pipe)
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("failed to start: %s", err.Error())
+		os.Exit(1)
+	}
+
+	wg.Add(1)
+
+	go func(reader *bufio.Reader, lineChan chan string) {
+		for {
+			line, err := reader.ReadBytes(SEP)
+			if err != nil {
+				fmt.Printf("failed to read: %s", err.Error())
+				break
+			}
+			lineChan <- string(line)
+		}
+		close(lineChan)
+		wg.Done()
+	}(reader, lineChan)
+
+	wg.Add(1)
+
+	go func(cmd *exec.Cmd) {
+		_ = cmd.Wait()
+		wg.Done()
+	}(cmd)
+
+	for line := range lineChan {
+		fmt.Println(line)
+	}
+
+	wg.Wait()
+
+	os.Exit(0)
+}
---
+++ test/perf/perf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+_len = 512*1024
+
+print("hello perf")
+
+for i in range(_len):
+    print("a" * _len + "\n")
+
+print("hello end")
