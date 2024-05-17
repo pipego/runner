f3765f9cb6c867b52aa25ec908c1e0fe16cef8d6
Jia <49358172+craftslab@users.noreply.github.com>
2024-05-17 11:14:44 +0000

    feat(language): add support for language and image

--- image/groovy/build.sh
+++ image/groovy/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# docker run docker run --rm -v /path/to/workspace:/workspace pipego/runner/language/groovy:latest /workspace/jenkinsfile
 docker build -f Dockerfile --rm -t pipego/runner/language/groovy:latest .
